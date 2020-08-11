711d00ead8461ac921ae22e7f37c35486489584d
InnaYF <inna@yachtfocus.com>
2020-08-11 15:47:10 +0200

    routing in api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataBaseUpdateController;
+use App\Http\Controllers\FlightController;
+use App\Http\Controllers\AirportController;
+use App\Http\Controllers\AirlineController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/data_update', 'DataBaseUpdateController');
+
+Route::apiResource('/flights', 'FlightController');
+Route::apiResource('/airports', 'AirportController');
+Route::apiResource('/airports/={id}', 'AirportController', ['airports' => 'id']);
+Route::apiResource('/airlines', 'AirlineController');
+
+
+Route::resource('/flights', 'FlightController')->only([
+    'index', 'show', 'store', 'update', 'destroy'
+]);
+
+Route::resource('/airports', 'AirportController')->only([
+    'index', 'show', 'store', 'update', 'destroy'
+]);
+
+Route::resource('/airlines', 'AirlineController')->only([
+    'index', 'show', 'store', 'update', 'destroy'
+]);
