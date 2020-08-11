f8757a986d54dc69984ccaa19352003546ba0337
InnaYF <inna@yachtfocus.com>
2020-08-11 15:46:04 +0200

    create controllers

---
+++ app/Http/Controllers/AirlineController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Airline;
+use Illuminate\Http\Request;
+
+class AirlineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Airline $airlines
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Airline $airlines)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Airline $airlines
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Airline $airlines)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Airline             $airlines
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Airline $airlines)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Airline  $airlines
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Airline $airlines)
+    {
+        //
+    }
+}
