fc803a1f3a306ff1fba7bc5bd850a99abc462c9e
InnaYF <inna@yachtfocus.com>
2020-08-12 20:20:10 +0200

    Middleware for mapping models and filing DB (maybe use not correct as middleware for http)

---
+++ app/Http/Middleware/MapperDBMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Airline;
+use App\Flight;
+use App\Airport;
+use App\Enums\ModelType;
+
+class MapperDBMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @param  \App\Enums\ModelType     $modelType
+     *
+     * @return mixed
+     */
+
+    public function handle($request, Closure $next, int $modelType)
+    {
+
+        $response_array = json_decode($request->getBody(), true);
+
+        /** @var Airline $airline */
+        /** @var Flight $flight */
+        /** @var Airline $airport */
+
+        foreach ($response_array as $response) {
+
+                switch($modelType) {
+                case ModelType::Airline :
+                    $model =  new Airline();
+                    $model->id = $response['id'];
+                    $model->name = $response['name'];
+                    $model->save();
+                    break;
+                case ModelType::Flight :
+                    $model =  new Flight();
+                    $model->airlineId = $response['airlineId'];
+                    $model->flightNumber = $response['flightNumber'];
+                    $model->departureAirportId = $response['departureAirportId'];
+                    $model->arrivalAirportId = $response['arrivalAirportId'];
+                    $model->save();
+                    break;
+                case ModelType::Airport :
+                    $model =  new Airport();
+                    $model->id = $response['id'];
+                    $model->latitude = $response['latitude'];
+                    $model->longitude = $response['longitude'];
+                    $model->name = $response['name'];
+                    $model->city = $response['city'];
+                    $model->countryId = $response['countryId'];
+                    $model->save();
+                    break;
+                };
+
+                }
+
+        return $next($request);
+    }
+
+
+}
