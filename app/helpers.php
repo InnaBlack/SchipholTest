e87d38a727a74631c46772f4a8581b5d19035c3c
InnaYF <inna@yachtfocus.com>
2020-08-13 10:09:06 +0200

    custom helper

---
+++ app/helpers.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inna
+ * Date: 2020-08-13
+ * Time: 10:00
+ */
+
+
+function distance($lat1, $lon1, $lat2, $lon2, $distanceUnit) {
+    if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+        return 0;
+    }
+    else {
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $unit = strtoupper($distanceUnit);
+
+        if ($unit == "K") {
+            return ($miles * 1.609344);
+        } else if ($unit == "N") {
+            return ($miles * 0.8684);
+        } else {
+            return $miles;
+        }
+    }
+}
